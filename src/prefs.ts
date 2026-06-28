3852fd54f66a2baf310a9440fcab97989a75dc95
Sebastian Lobbe <slobbe@lobbe.cc>
2026-06-28 23:28:05 +0200

    Polish Codex reset UI

--- src/prefs.ts
+++ src/prefs.ts
@@ -1,3 +1,5 @@
+import Gdk from "gi://Gdk";
+import Gtk from "gi://Gtk";
 import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 import { AboutPage, CodexPage, DisplayPage } from "./ui/preferences-pages.js";
@@ -5,6 +7,11 @@ import { AboutPage, CodexPage, DisplayPage } from "./ui/preferences-pages.js";
 export default class CodexMeterPreferences extends ExtensionPreferences {
     async fillPreferencesWindow(window) {
         const settings = this.getSettings();
+        const display = Gdk.Display.get_default();
+
+        if (display) {
+            Gtk.IconTheme.get_for_display(display).add_search_path(`${this.path}/icons`);
+        }
 
         window.add(new (DisplayPage as any)(settings));
         window.add(new (CodexPage as any)());
